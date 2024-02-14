f7f17521e6f078ffcafdd209069b2da506f9985a
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2024-02-14 16:29:52 +0100

    Added basic CMake files and file structure.
    The CMake files are very basic at this point of time.
    The configure, make and test steps execute successfully.
    The directory for examples will be added later.
    The source files are just placeholders to test CMake build.

---
+++ cmake/config.cmake
@@ -0,0 +1,34 @@
+
+include(CheckIncludeFile)
+include(CheckIncludeFileCXX)
+include(ProcessorCount)
+include(CheckTypeSize)
+
+# Generic routine to set appropriate cores for all tests.
+# Typically the tests are executed with maximum 2 cores.
+cmake_host_system_information(RESULT Ncpu QUERY NUMBER_OF_PHYSICAL_CORES)
+if(Ncpu LESS 2)
+  ProcessorCount(n)
+  if(n GREATER Ncpu)
+    set(Ncpu ${n})
+  endif()
+  set(MPIEXEC_NUMPROC_MAX 1)
+else()
+  set(MPIEXEC_NUMPROC_MAX 2)
+endif()
+
+# Check for primitive data types.
+check_type_size(float SIZEOF_FLOAT)
+check_type_size(double SIZEOF_DOUBLE)
+message(STATUS "Size of float - ${SIZEOF_FLOAT} bytes.")
+message(STATUS "Size of double - ${SIZEOF_DOUBLE} bytes.")
+
+# Check for headers.
+if(${MPI_FOUND})
+  check_include_file(mpi.h HAVE_MPI_H)
+endif()
+check_include_file(stdint.h HAVE_STDINT_H)
+check_include_file(math.h HAVE_MATH_H)
+check_include_file_cxx("memory" HAVE_MEMORY_H)
+check_include_file_cxx("algorithm" HAVE_ALGORITHM_H)
+check_include_file_cxx("vector" HAVE_VECTOR_H)
---
+++ src/biomesh_vtk.hpp
@@ -0,0 +1,10 @@
+
+#ifndef BIOMESH_VTK_HPP
+#define BIOMESH_VTK_HPP
+
+class biomesh_vtk
+{
+
+};
+
+#endif
