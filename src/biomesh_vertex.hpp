b6faec5bb5ee7181c4bf77723dbbcc8a846be4b3
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2024-05-22 18:47:32 +0200

    Implemented vertex class.

--- src/biomesh_vertex.hpp
+++ src/biomesh_vertex.hpp
@@ -2,6 +2,11 @@
 #ifndef BIOMESH_VERTEX_HPP
 #define BIOMESH_VERTEX_HPP
 
+#include <biomesh_base.hpp>
+#include <cmath>
+
+namespace biomesh
+{
 /**
  * @brief Class for cartesian coordinates.
  */
@@ -15,6 +20,10 @@ public:
 
   /**
    * Custom constructor.
+   *
+   * @param[in] x The x coordiante.
+   * @param[in] y The y coordiante.
+   * @param[in] z The z coordiante.
    */
   vertex (double x, double y
 #ifdef BIOMESH_ENABLE_3D
@@ -25,11 +34,24 @@ public:
 
   /**
    * Getter for co-ordinate in the X,Y,Z direction.
+   *
+   * Set dir to 'x' for x coordinate.
+   * Set dir to 'y' for y coordinate.
+   * Set dir to 'z' for z coordinate.
+   *
+   * @param[in] dir The coordinate direction.
    */
-  double operator() (char dir);
+  double operator() (char dir) const;
+
+  /**
+   * Comparator for two vertices.
+   *
+   * @param[in] other The reference vertex for comparison.
+   */
+  bool operator== (const vertex &other) const;
 
 private:
   double m_x, m_y, m_z; /* Cartesian coordinates. */
 };
-
+} // namespace biomesh
 #endif
