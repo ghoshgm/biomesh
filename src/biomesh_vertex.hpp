f91f8aeda84e8c21b337bfcf8633fe2480778bcf
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2024-05-22 12:16:47 +0200

    Added class for cartesian coordinates.

---
+++ src/biomesh_vertex.hpp
@@ -0,0 +1,35 @@
+
+#ifndef BIOMESH_VERTEX_HPP
+#define BIOMESH_VERTEX_HPP
+
+/**
+ * @brief Class for cartesian coordinates.
+*/
+class vertex
+{
+public:
+  /**
+   * Default constructor.
+  */
+  vertex();
+
+  /**
+   * Custom constructor.
+  */
+  vertex(double x, double y
+#ifdef BIOMESH_ENABLE_3D
+    ,double z
+#endif
+  );
+
+  /**
+   * Getter for co-ordinate in the X,Y,Z direction.
+  */
+  double operator()(char dir);
+
+private:
+  double m_x, m_y, m_z; /* Cartesian coordinates. */
+};
+
+
+#endif
