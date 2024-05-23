ddb5bcd9178d96ce906e830d3a9f95cb7c280cf9
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2024-05-23 13:48:09 +0200

    The biomesh.hpp file holds only muscle class.
    The muscle class will be composed for fiber, fiber_manager
    and mechanics_mesh class.

--- src/biomesh.hpp
+++ src/biomesh.hpp
@@ -3,52 +3,6 @@
 #define BIOMESH_HPP
 
 #include <biomesh_base.hpp>
-#include <biomesh_vertex.hpp>
-#include <vector>
-
-namespace biomesh
-{
-/**
- * @brief Class for modelling muscle fibers.
- *
- * Muscle fiber is represented as a 1D grid.
- * A fiber is modelled as an ordered set of cartesian coordiantes.
- *
- */
-class fiber
-{
-public:
-  /**
-   * Default constructor.
-   */
-  fiber ();
-
-  /**
-   * Copy constructor.
-   */
-  fiber (const fiber &other);
-
-  /**
-   * Move constructor.
-   */
-  fiber (fiber &&other);
-
-  /**
-   * Get the set of vertices for this fiber.
-   */
-  std::vector<vertex> get_vertices () const;
-
-  /**
-   * Get the vertex at a specified index.
-   */
-  vertex operator[] (int index) const;
-
-private:
-  std::vector<vertex> m_vset; /* Set of vertices. */
-};
-
-/* TODO: Implement fiber_grid, mechanics_grid, muscle */
-
-} // namespace biomesh
+#include <biomesh_fiber.hpp>
 
 #endif
