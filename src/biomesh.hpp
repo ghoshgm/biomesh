efce44008c6b5d5bb281a0f0f27f2ab0ccb863f7
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2024-06-11 11:33:23 +0200

    Minor refactors.

--- src/biomesh.hpp
+++ src/biomesh.hpp
@@ -2,7 +2,8 @@
 #ifndef BIOMESH_HPP
 #define BIOMESH_HPP
 
-#include <biomesh_fiber.hpp>
+#include <biomesh_fiber2d.hpp>
+#include <biomesh_fiber3d.hpp>
 #include <biomesh_fiber_manager.hpp>
 
 #endif
