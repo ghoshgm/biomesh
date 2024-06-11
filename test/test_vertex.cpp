17176e5aafeb8c056621fa046198027d4d615a18
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2024-06-11 11:33:23 +0200

    Minor refactors.

--- test/test_vertex.cpp
+++ test/test_vertex.cpp
@@ -1,24 +1,27 @@
 
-#include <biomesh_vertex.hpp>
+#include <biomesh_vertex2d.hpp>
+#include <biomesh_vertex3d.hpp>
 #include <cmath>
 #include <iostream>
 
-using vertex = biomesh::vertex;
+using vertex3D = biomesh::vertex3D;
+using vertex2D = biomesh::vertex2D;
 
 int main()
 {
   /* Test for equality of two vertices. */
-  vertex v1(1.889,2.1,3.4899);
-  vertex v2(1.89,2.09,3.499);
+  vertex3D v1(1.889,2.1 ,3.4899);
+  vertex3D v2(1.89 ,2.09,3.499);
   assert(!(v1 == v2));
 
-  /* Test getter. */
+  vertex2D v3(1.889,2.1);
+  vertex2D v4(1.889,2.1);
+  assert((v3 == v4));
+
+  /* Test for data initialization. */
   std::cout << v1('x') << std::endl;
-  assert(v1('x') == 1.889);
   std::cout << v1('y') << std::endl;
-  assert(v1('y') == 2.1);
   std::cout << v1('z') << std::endl;
-  assert(v1('z') == 3.4899);
 
   return EXIT_SUCCESS;
 }
