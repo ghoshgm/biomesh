408caf43b4ba5cf4d493dee50a8116b153b83c9b
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2024-06-11 11:44:00 +0200

    Added functions to fiber3D class.

--- src/biomesh_fiber3d.cpp
+++ src/biomesh_fiber3d.cpp
@@ -3,9 +3,27 @@
 
 namespace biomesh
 {
+fiber3D::fiber3D () {}
 
 fiber3D::fiber3D (const vertex3D &seed) : m_seed{ seed } {}
 
+fiber3D::fiber3D (const fiber3D &other)
+{
+  this->m_seed = other.m_seed;
+  this->m_fiber_vertices = other.m_fiber_vertices;
+}
+
+fiber3D::fiber3D (fiber3D &&other)
+{
+  this->m_seed = other.m_seed;
+  this->m_fiber_vertices = std::move (other.m_fiber_vertices);
+}
+
 fiber3D::~fiber3D () {}
+
+int
+fiber3D::generate_fiber ()
+{
+}
 
 } // namespace biomesh
