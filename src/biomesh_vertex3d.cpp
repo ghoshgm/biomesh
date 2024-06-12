c88f051943624ad9690d5157e2b7e1aebb8cc36f
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2024-06-12 21:39:17 +0200

    Vertex classes wrap Eigen3 fucntions.
    The vertex classes now use Eigen3 data structures for
    all functionalities. It takes away the effort of error
    checking and possible optimizations. The linear algebra
    routines would also be valuable in other areas.

--- src/biomesh_vertex3d.cpp
+++ src/biomesh_vertex3d.cpp
@@ -4,11 +4,13 @@
 namespace biomesh
 {
 
+vertex3D::vertex3D () {}
+
 vertex3D::vertex3D (double x, double y, double z)
 {
-  m_x = x;
-  m_y = y;
-  m_z = z;
+  m_position (0) = x;
+  m_position (1) = y;
+  m_position (2) = z;
 }
 
 vertex3D::~vertex3D () {}
