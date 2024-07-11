921f77fe5a75cfc55bcc4ff79777682ea2d89fe9
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2024-07-11 16:48:55 +0200

    Added class for json with unit test.

---
+++ src/biomesh_json_parser.cpp
@@ -0,0 +1,35 @@
+
+#include "biomesh_json_parser.hpp"
+
+namespace biomesh
+{
+
+json_parser::json_parser (const std::string &file_name)
+    : m_file_name{ file_name }
+{
+}
+
+json_parser::~json_parser () {}
+
+int
+json_parser::read ()
+{
+  std::ifstream m_file (m_file_name);
+  BIOMESH_ASSERT (m_file.is_open ());
+
+  Json::Reader reader;
+  bool success = reader.parse (m_file, m_data);
+  BIOMESH_ASSERT (success == true);
+
+  m_file.close ();
+
+  return (success == true) ? BIOMESH_SUCCESS : BIOMESH_ERROR;
+}
+
+Json::Value
+json_parser::get_json_data () const
+{
+  return m_data;
+}
+
+}
