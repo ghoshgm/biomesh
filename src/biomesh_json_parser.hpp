35f8e9227cbe93742652a6eb34aeafb405c5b541
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2024-07-15 13:18:17 +0200

    Added documentation for json parser.

--- src/biomesh_json_parser.hpp
+++ src/biomesh_json_parser.hpp
@@ -18,12 +18,26 @@ private:
   std::string m_file_name;
 
 public:
+  /**
+   * Constructor.
+   *
+   * @param[in] file_name The path to json file.
+   */
   json_parser (const std::string &file_name);
 
+  /**
+   * Destructor.
+   */
   ~json_parser ();
 
+  /**
+   * Function to read JSON file and store the data.
+   */
   int read ();
 
+  /**
+   * Function to retrive the data object obtained from the JSON file.
+   */
   Json::Value get_json_data () const;
 };
 
