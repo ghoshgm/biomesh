581a8b7543434413dc9f367cfca39cff0aa3f2d0
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2024-08-28 21:05:08 +0200

    Separated header from source for template classes.

--- src/biomesh_fiber_grid.hpp
+++ src/biomesh_fiber_grid.hpp
@@ -62,60 +62,7 @@ private:
   std::vector<fiber> m_fiber_set;
 };
 
-template <class fiber, class vertex>
-inline fiber_grid<fiber, vertex>::fiber_grid (const std::string &file_name)
-    : m_jparser{ file_name }
-{
-  m_fiber_count = 0;
-}
+#include "biomesh_fiber_grid.tpp"
 
-template <class fiber, class vertex>
-inline int
-fiber_grid<fiber, vertex>::generate_fiber_grid (const vector_field &vfield,
-                                                size_t fpoint_count)
-{
-  int ret = m_jparser.read ();
-  BIOMESH_ASSERT (ret == true);
-  auto data = m_jparser.get_json_string ();
-  m_fiber_count = data["seed_points"].size ();
-  BIOMESH_ASSERT (this->m_fiber_count > 0);
-
-  /* Loop over the fibers. */
-  for (size_t fcount = 0; fcount < this->m_fiber_count; ++fcount)
-    {
-      /* Obtain the seed point. */
-      double x = (data["seed_points"][(unsigned)fcount]["x"]).asDouble ();
-      double y = (data["seed_points"][(unsigned)fcount]["y"]).asDouble ();
-      vertex seed (x, y);
-
-      /* Initialize the fiber. */
-      fiber f (seed, fpoint_count);
-
-      /* Generate fiber. */
-      f.generate_fiber (vfield);
-
-      /* Push fiber to fiber grid. */
-      m_fiber_set.emplace_back (f);
-    }
-
-  /** TODO: Add check for testing validity of the fiber grid.
-   * Return BIOMESH_SUCCESS iff the fiber grid is valid.
-   */
-  return BIOMESH_SUCCESS;
-}
-
-template <class fiber, class vertex>
-inline fiber
-fiber_grid<fiber, vertex>::operator[] (int idx) const
-{
-  return m_fiber_set[idx];
-}
-
-template <class fiber, class vertex>
-inline size_t
-fiber_grid<fiber, vertex>::size () const
-{
-  return m_fiber_set.size ();
-}
 } // namespace biomesh
 #endif
