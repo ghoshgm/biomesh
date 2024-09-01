7839372d20c4d9c97707b501f8036eb65a4b452f
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2024-09-01 21:00:25 +0200

    Added examples subdir.

---
+++ examples/unit_square_fibers.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+#include <biomesh_vector_field.hpp>
+#include <biomesh_json_parser.hpp>
+#include <biomesh_fiber_grid.hpp>
+#include <biomesh_visualization.hpp>
+
+using namespace biomesh;
+
+int main(int argc, char** argv)
+{
+  /* Load vector field from VTK file. */
+  vector_field field(argv[1]);
+  field.load_vtk_grid();
+
+  /* Generate fibers. */
+  fiber_grid<fiber2D,vertex2D> f(argv[2]);
+  f.generate_fiber_grid(field, 22, 0.2);
+
+  /* Write fibers to JSON format. */
+  json_parser jp;
+  jp.export_fiber_grid_json< fiber_grid<fiber2D,vertex2D> >(f,"test.json");
+
+  /* Write fibers to VTK format. */
+  visualization::export_fiber_grid_vtk< fiber_grid<fiber2D,vertex2D> >(f);
+
+  return EXIT_SUCCESS;
+}
