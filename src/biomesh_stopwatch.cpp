1d2114ba29e95f0f8471575a7b9cba925070c0f7
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2024-09-13 19:16:07 +0200

    Implemented stopwatch class.

---
+++ src/biomesh_stopwatch.cpp
@@ -0,0 +1,26 @@
+
+#include "biomesh_stopwatch.hpp"
+
+namespace biomesh
+{
+
+stopwatch::stopwatch () {}
+
+stopwatch::~stopwatch () {}
+
+void
+stopwatch::start ()
+{
+  m_start = std::chrono::high_resolution_clock::now ();
+}
+
+void
+stopwatch::end ()
+{
+  m_end = std::chrono::high_resolution_clock::now ();
+  auto span = std::chrono::duration_cast<
+      std::chrono::duration<double, std::milli> > (m_end - m_start);
+  BIOMESH_LINFO (0, "Time elasped = " + std::to_string (span.count ()) + "ms");
+}
+
+}
