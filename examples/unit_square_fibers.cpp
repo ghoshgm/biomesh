0bc06db1fecd3d0391b5f32668909b1d740678d5
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2024-09-18 11:09:19 +0200

    Placeholder commit.

--- examples/unit_square_fibers.cpp
+++ examples/unit_square_fibers.cpp
@@ -9,17 +9,23 @@ using namespace biomesh;
 
 int main(int argc, char** argv)
 {
+#if 0
+  double lenx = 12;
+  int vertex_count = 100;
+  double mesh_width = lenx / (double)vertex_count;
+#endif
+
   /* Load vector field from VTK file. */
   vector_field field(argv[1]);
   field.load_vtk_grid();
 
   /* Generate fibers. */
   fiber_grid<fiber2D,vertex2D> f(argv[2]);
-  f.generate_fiber_grid(field, 22, 0.2);
+  f.generate_fiber_grid(field, 10, 0.0001);
 
   /* Write fibers to JSON format. */
   json_parser jp;
-  jp.export_fiber_grid_json< fiber_grid<fiber2D,vertex2D> >(f,"test.json");
+  jp.export_fiber_grid_json< fiber_grid<fiber2D,vertex2D> >(f,"cuboid_fibers.json");
 
   /* Write fibers to VTK format. */
   visualization::export_fiber_grid_vtk< fiber_grid<fiber2D,vertex2D> >(f);
--- test/test_fiber_grid.cpp
+++ test/test_fiber_grid.cpp
@@ -15,7 +15,7 @@ int main(int argc, char** argv)
 
   /* Generate fibers. */
   fiber_grid<fiber2D,vertex2D> f(argv[2]);
-  f.generate_fiber_grid(field, 22, 0.2);
+  f.generate_fiber_grid(field, 4, 0.02);
 
   /* Write fibers to JSON format. */
   json_parser jp;
