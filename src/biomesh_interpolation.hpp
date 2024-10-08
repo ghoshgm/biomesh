c2560b08f5d0eadbf12fae65a0a5ae36b80905fa
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2024-10-08 21:22:53 +0200

    Reimplemented interpolation functions
    It is more elegant to do bilinear and
    trilinear interpolation using weights.
    The older version will be phased out as
    the new functions are integrated into
    the fiber generation algorithm.

--- src/biomesh_interpolation.hpp
+++ src/biomesh_interpolation.hpp
@@ -38,6 +38,47 @@ double bilinear (const vertex2D &vertex_min, const vertex2D &vertex_max,
                  const vertex2D &probe, double scalar1, double scalar2,
                  double scalar3, double scalar4);
 
+/**
+ * Function for bilinear interpolation on an unit square.
+ *
+ * The vertex ordering is conformal to VTK_QUAD ordering.
+ *
+ * The input vertex 'probe' must be in the parametric space.
+ *
+ * @param[in] probe The vertex at which interpolation needs to be performed.
+ * @param[in] scalar0 The scalar magnitude at vertex 0.
+ * @param[in] scalar1 The scalar magnitude at vertex 1.
+ * @param[in] scalar2 The scalar magnitude at vertex 2.
+ * @param[in] scalar3 The scalar magnitude at vertex 3.
+ *
+ * @return The interpolated scalar at the probe.
+ */
+double bilinear (const vertex2D &probe, double scalar0, double scalar1,
+                 double scalar2, double scalar3);
+
+/**
+ * Function for trilinear interpolation on an unit cube.
+ *
+ * The vertex ordering is conformal to VTK_HEXAHEDRON ordering.
+ *
+ * The input vertex 'probe' must be in the parametric space.
+ *
+ * @param[in] probe The vertex at which interpolation needs to be performed.
+ * @param[in] scalar0 The scalar magnitude at vertex 0.
+ * @param[in] scalar1 The scalar magnitude at vertex 1.
+ * @param[in] scalar2 The scalar magnitude at vertex 2.
+ * @param[in] scalar3 The scalar magnitude at vertex 3.
+ * @param[in] scalar4 The scalar magnitude at vertex 4.
+ * @param[in] scalar5 The scalar magnitude at vertex 5.
+ * @param[in] scalar6 The scalar magnitude at vertex 6.
+ * @param[in] scalar7 The scalar magnitude at vertex 7.
+ *
+ * @return The interpolated scalar at the probe.
+ */
+double trilinear (const vertex3D &probe, double scalar0, double scalar1,
+                  double scalar2, double scalar3, double scalar4,
+                  double scalar5, double scalar6, double scalar7);
+
 } // namespace interpolation.
 
 } // namespace biomesh
--- test/test_interpolation.cpp
+++ test/test_interpolation.cpp
@@ -1,9 +1,10 @@
 
-#include <biomesh_interpolation.cpp>
+#include <biomesh_interpolation.hpp>
 #include <cassert>
 #include <cmath>
 
 using vertex2D = biomesh::vertex2D;
+using vertex3D = biomesh::vertex3D;
 
 int main()
 {
@@ -40,9 +41,42 @@ int main()
 
     /**
      * I did not put an assertion here because comparing floats/doubles is a headache.
-    */
+     */
     std::cout << "Results for Test 2." << std::endl;
     std::cout << "Reference value = " << actual_val << " Interpolated val = " << iterp_val << std::endl;
+  }
+
+  {
+    vertex2D probe(0.5, 0.5);
+    double s1 = 10;
+    double s2 = 20;
+    double s3 = 30;
+    double s4 = 40;
+
+    double actual_val = 100 / 4;
+    double iterp_val = biomesh::interpolation::bilinear(probe, s1, s2, s3, s4);
+
+    std::cout << actual_val << std::endl;
+    std::cout << iterp_val << std::endl;
+    assert(actual_val == iterp_val);
+  }
+
+  {
+    vertex3D probe(0.5, 0.5, 0.5);
+    double s1 = 10;
+    double s2 = 20;
+    double s3 = 30;
+    double s4 = 40;
+    double s5 = 50;
+    double s6 = 60;
+    double s7 = 70;
+    double s8 = 80;
+
+    double actual_val = 0.125 * (s1 + s2 + s3 + s4 + s5 + s6 + s7 + s8);
+    double res = biomesh::interpolation::trilinear(probe, s1, s2, s3, s4, s5, s6, s7, s8);
+    std::cout << res << std::endl;
+    std::cout << actual_val << std::endl;
+    assert(res == actual_val);
   }
 
   return EXIT_SUCCESS;
