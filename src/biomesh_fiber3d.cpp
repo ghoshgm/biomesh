577cd83e3388e08dcae143fa91795e605131f4f1
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2024-10-10 19:04:10 +0200

    Refactored fiber3D class interface.

--- src/biomesh_fiber3d.cpp
+++ src/biomesh_fiber3d.cpp
@@ -3,14 +3,15 @@
 
 namespace biomesh
 {
-fiber3D::fiber3D (size_t gpoint_count) : m_gpoint_count{ gpoint_count }
+fiber3D::fiber3D (size_t gpoint_count, double width)
+    : m_gpoint_count{ gpoint_count }, m_width{ width }
 {
   m_fiber_vertices.reserve (m_gpoint_count);
   m_fiber_vertices.emplace_back (m_seed);
 }
 
-fiber3D::fiber3D (const vertex3D &seed, size_t gpoint_count)
-    : m_seed{ seed }, m_gpoint_count{ gpoint_count }
+fiber3D::fiber3D (const vertex3D &seed, size_t gpoint_count, double width)
+    : m_seed{ seed }, m_gpoint_count{ gpoint_count }, m_width{ width }
 {
   m_fiber_vertices.reserve (m_gpoint_count);
   m_fiber_vertices.emplace_back (m_seed);
@@ -30,21 +31,47 @@ fiber3D::fiber3D (fiber3D &&other)
 
 fiber3D::~fiber3D () {}
 
-int
-fiber3D::generate_fiber ()
+void
+fiber3D::generate_fiber (const vector_field &vfield)
 {
+  /* Obtain the structured grid. */
+  auto sgrid = vfield.get_grid ();
+  BIOMESH_ASSERT (sgrid != nullptr);
+
+  BIOMESH_LINFO (0, "Fiber begin.");
+
+  int subid;
+  double pcoords[3];
+  double weights[VTK_CELL_SIZE];
+
+  int arridx = 1;
+  vtkDataArray *da = sgrid->GetPointData ()->GetArray ("vectors", arridx);
+  BIOMESH_ASSERT (da != nullptr);
+
+  double point[3] = { m_seed ('x'), m_seed ('y'), m_seed ('z') };
+
+  vertex3D next = m_seed;
+  for (int ii = 0; ii < m_gpoint_count; ++ii)
+    {
+      auto cellid
+          = sgrid->FindCell (point, nullptr, -1, 0, subid, pcoords, weights);
+
+      std::cout << "---------" << cellid << std::endl;
+    }
+
+  BIOMESH_LINFO (0, "Fiber end.");
 }
 
 size_t
 fiber3D::size () const
 {
-  return m_gpoint_count;
+  return m_fiber_vertices.size ();
 }
 
 vertex3D
 fiber3D::operator[] (int idx) const
 {
-  BIOMESH_ASSERT (idx >= 0 && idx < m_gpoint_count);
+  BIOMESH_ASSERT (idx >= 0 && idx < m_fiber_vertices.size ());
   return m_fiber_vertices[idx];
 }
 
