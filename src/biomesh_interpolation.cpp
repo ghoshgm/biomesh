2119e9107e366db962d0338499152333c0a04c03
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2024-10-14 17:57:45 +0200

    Fixed float comparison issue.

--- src/biomesh_interpolation.cpp
+++ src/biomesh_interpolation.cpp
@@ -20,7 +20,7 @@ bilinear (const vertex2D &probe, std::array<double, 4> &scalars)
   double w1 = probe ('x') * (1.0 - probe ('y'));
   double w2 = (1.0 - probe ('x')) * probe ('y');
   double w3 = probe ('x') * probe ('y');
-  BIOMESH_ASSERT ((w0 + w1 + w2 + w3) == 1.0);
+  BIOMESH_ASSERT (std::abs ((w0 + w1 + w2 + w3) - 1.0) < 1e-8);
 
   /* Interpolate. */
   interp_res = (w0 * scalars[0]) + (w1 * scalars[1]) + (w2 * scalars[2])
@@ -47,7 +47,8 @@ trilinear (const vertex3D &probe, std::array<double, 8> &scalars)
   double w5 = probe ('x') * (1.0 - probe ('y')) * probe ('z');
   double w6 = probe ('x') * probe ('y') * probe ('z');
   double w7 = (1.0 - probe ('x')) * probe ('y') * probe ('z');
-  BIOMESH_ASSERT ((w0 + w1 + w2 + w3 + w4 + w5 + w6 + w7) == 1.0);
+  BIOMESH_ASSERT (std::abs ((w0 + w1 + w2 + w3 + w4 + w5 + w6 + w7) - 1.0)
+                  < 1e-8);
 
   /* Interpolate. */
   interp_res = (w0 * scalars[0]) + (w1 * scalars[1]) + (w2 * scalars[2])
