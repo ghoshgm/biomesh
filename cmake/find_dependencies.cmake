64deaef09155b7493395b92cca34792733461a0b
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2024-10-27 14:12:39 +0100

    Added CMake support for PETSc.

--- cmake/find_dependencies.cmake
+++ cmake/find_dependencies.cmake
@@ -14,5 +14,10 @@ if(${Eigen3_FOUND})
   message(STATUS "Found Eigen3: ${Eigen3_DIR} (found version ${Eigen3_VERSION})")
 endif()
 
+pkg_check_modules(PETSC REQUIRED PETSc)
+if(${PETSC_FOUND})
+  message(STATUS "PETSc path: ${PETSC_LIBDIR}")
+endif()
+
 pkg_check_modules(JSONCPP REQUIRED jsoncpp)
 message(STATUS "Found jsoncpp: ${JSONCPP_LIBDIR}")
