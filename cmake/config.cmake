cc26ed1c79cf8924dcf40a4199d9d16a72577036
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2024-11-25 16:21:55 +0100

    Remove MPI dependency.

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -6,31 +6,11 @@ include(CheckCXXSymbolExists)
 include(ProcessorCount)
 include(CheckTypeSize)
 
-# Generic routine to set appropriate cores for all tests.
-# Typically the tests are executed with maximum 2 cores.
-cmake_host_system_information(RESULT Ncpu QUERY NUMBER_OF_PHYSICAL_CORES)
-if(Ncpu LESS 2)
-  ProcessorCount(n)
-  if(n GREATER Ncpu)
-    set(Ncpu ${n})
-  endif()
-  set(MPIEXEC_NUMPROC_MAX 1)
-else()
-  set(MPIEXEC_NUMPROC_MAX 2)
-endif()
-
 # Check for primitive data types.
 check_type_size(float SIZEOF_FLOAT)
 check_type_size(double SIZEOF_DOUBLE)
 message(STATUS "Size of float - ${SIZEOF_FLOAT} bytes.")
 message(STATUS "Size of double - ${SIZEOF_DOUBLE} bytes.")
-
-# Check for headers.
-if(${MPI_FOUND})
-  set(CMAKE_REQUIRED_LIBRARIES MPI::MPI_CXX)
-  check_include_file(mpi.h HAVE_MPI_H)
-  set(CMAKE_REQUIRED_LIBRARIES)
-endif()
 
 check_include_file(stdint.h HAVE_STDINT_H)
 check_include_file(math.h HAVE_MATH_H)
@@ -40,18 +20,11 @@ check_include_file_cxx("vector" HAVE_VECTOR_H)
 
 # Check for functions.
 set(CMAKE_REQUIRED_LIBRARIES m)
-check_symbol_exists(sqrt math.h HAVE_SQRT)
 check_symbol_exists(fabs math.h HAVE_FABS)
 
 # Set macros for configure macros.
-if(ENABLE_MPI)
-  set(BIOMESH_ENABLE_MPI 1)
-  set(BIOMESH_CC \"${MPI_C_COMPILER}\")
-  set(BIOMESH_CPP \"${MPI_CXX_COMPILER}\")
-else()
-  set(BIOMESH_CC \"${CMAKE_C_COMPILER}\")
-  set(BIOMESH_CPP \"${CMAKE_CXX_COMPILER}\")
-endif()
+set(BIOMESH_CC \"${CMAKE_C_COMPILER}\")
+set(BIOMESH_CPP \"${CMAKE_CXX_COMPILER}\")
 
 set(BIOMESH_CPPFLAGS \"${CMAKE_CXX_FLAGS}\")
 
