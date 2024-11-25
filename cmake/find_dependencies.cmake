e327dada8a381514a231208914e40275a20b91b0
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2024-11-25 16:21:55 +0100

    Remove MPI dependency.

--- cmake/find_dependencies.cmake
+++ cmake/find_dependencies.cmake
@@ -1,9 +1,5 @@
 include(FindPkgConfig)
 
-if(ENABLE_MPI)
- find_package(MPI REQUIRED)
-endif()
-
 find_package(VTK REQUIRED)
 if(${VTK_FOUND})
   message(STATUS "Found VTK: ${VTK_DIR} (found version ${VTK_VERSION})")
