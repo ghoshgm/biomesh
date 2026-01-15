c7f009ab3f79e259c0fccef441fb791b92f6a864
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2026-01-15 13:06:27 +0100

    Minor refactor.

--- src/biomesh_cell_table.hpp
+++ src/biomesh_cell_table.hpp
@@ -49,7 +49,6 @@ public:
   int operator[] (size_t cell_index) const;
 
 private:
-  // vtkStructuredGrid *sgrid;
   std::vector<int> m_boundary_cell_index;
   std::vector<int> m_seed_cell_index;
   std::vector<int> m_cell_type;
