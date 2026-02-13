33dc84e984ac2dba6a0e1997e024c0022bd19ba1
Gautam Ghosh <gautam.ghosh@ipvs.uni-stuttgart.de>
2026-02-13 22:57:51 +0100

    Updated examples.

--- examples/tube_fibers/tube_fibers.cpp
+++ examples/tube_fibers/tube_fibers.cpp
@@ -15,7 +15,8 @@ main (int argc, char **argv)
 
   /* Generate fibers. */
   fiber_grid3d f (argv[3]);
-  f.generate_fiber_grid (field);
+  int ret = f.generate_fiber_grid (field);
+  BIOMESH_ASSERT(ret == BIOMESH_SUCCESS);
 
   /* Write fibers to JSON format. */
   json_parser jp1;
